d53600653a578e170307a59013af0df2fcaabd3b
Tamara Silva <tamy.s.design@gmail.com>
2022-08-08 11:22:28 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -33,10 +33,15 @@ function Listing() {
 
 
 
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber (newPageNumber);
+    }
+
+
     return (
 
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange} />
 
             <div className="container">
                 <div className="row">
